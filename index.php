0872a24e71833edf7683d57e01fd84bfdc754720
Hapexi <asptomi@gmail.com>
2013-06-08 10:18:55 +0300

    Tiedostojen alustaminen

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NRA Finland</title>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+</head>
+<body>
+  <div id="header-contact">
+    <div id="logo"></div>
+    <div id="contact">
+      <div id="email-info">
+        <p><span>Yleisposti:</span>     info@nra.fi</p>
+        <p><span>Jäsenasiat:</span>     jasenasiat@nra.fi</p>
+        <p><span>Tiedottaja:</span>     tiedotus@nra.fi</p>
+      </div>
+      <div id="post-info">
+        <p>NRA RY </br>
+           PL 419 </br>
+           00151 Helsinki </br></p>
+      </div>
+    </div>
+  </div>
+  <div id="menu"></div>
+  <div id="content-slider"><img src="http://placehold.it/1903x680"></div>
+</body>
+</html>
